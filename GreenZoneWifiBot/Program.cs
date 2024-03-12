e75723e0f8c025c0aa2ab4f1840861890d54995a
Тазеев Алмаз <tyj5resd@gmail.com>
2024-03-12 19:52:13 +0300

    .

---
+++ GreenZoneWifiBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using dotenv.net;
+
+namespace GreenZoneWifiBot;
+
+public static class Solution
+{
+    public static async Task Main()
+    {
+        ConfigureApplication();
+
+        var tgBotToken = Environment.GetEnvironmentVariable("TgBotToken") ?? "";
+        var botClient = new TelegramBotClient(tgBotToken);
+        var me = await botClient.GetMeAsync();
+        Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+    }
+
+    private static void ConfigureApplication()
+    {
+        DotEnv.Load();
+    }
+}
